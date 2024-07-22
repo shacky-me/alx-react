66bf23a4f384d372eb1279e0caf8b0d7bd8b9add
shacky-me <meshackkiprono12@gmail.com>
2024-07-22 19:06:22 +0300

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
-import $ from 'jquery';
+'use strict';
+const $ = require('jquery');
 
-$(document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
